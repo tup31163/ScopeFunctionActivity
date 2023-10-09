64d436bdd14c067809ebf1ff06983268c76afce0
Kenny Miller <63763541+knnymllr@users.noreply.github.com>
2023-10-09 15:42:46 -0400

    Convert third function to single expression

--- app/src/main/java/edu/temple/scopefunctionactivity/MainActivity.kt
+++ app/src/main/java/edu/temple/scopefunctionactivity/MainActivity.kt
@@ -37,20 +37,12 @@ class MainActivity : AppCompatActivity() {
     }
 
     // Create a view from an item in a collection, but recycle if possible (similar to an AdapterView's adapter)
-    private fun getView(position: Int, recycledView: View?, collection: List<Int>, context: Context): View {
-        val textView: TextView
 
-        if (recycledView != null) {
-            textView = recycledView as TextView
-        } else {
-            textView = TextView(context)
-            textView.setPadding(5, 10, 10, 0)
-            textView.textSize = 22f
-        }
-
-        textView.text = collection[position].toString()
-
-        return textView
+    private fun getView(position: Int, recycledView: View?, collection: List<Int>, context: Context) : View = if (recycledView != null) {
+        TextView(this).apply { recycledView as TextView }.apply {this.text = collection[position].toString()}
+    } else {
+        TextView(context).apply {this.setPadding(5,10,10,0)}.apply {this.textSize = 22f}.apply {this.text = collection[position].toString()}
     }
+
 
 }
