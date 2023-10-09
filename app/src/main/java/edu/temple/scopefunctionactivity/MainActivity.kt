64f949f3416f627e70c9773080a15be4ddd1fc3d
Kenny Miller <63763541+knnymllr@users.noreply.github.com>
2023-10-09 15:31:14 -0400

    Convert second function to single expression

--- app/src/main/java/edu/temple/scopefunctionactivity/MainActivity.kt
+++ app/src/main/java/edu/temple/scopefunctionactivity/MainActivity.kt
@@ -27,22 +27,13 @@ class MainActivity : AppCompatActivity() {
     // Look at the final/return value and build the function "working backwards"
 
     // Return a list of random, sorted integers
-    private fun getTestDataArray() : List<Int> {
-        val testArray = MutableList(10){ Random.nextInt()}
-        testArray.sort()
-        return testArray
-    }
+    private fun getTestDataArray() = MutableList(10){ Random.nextInt()}.sort()
 
     // Return true if average value in list is greater than median value, false otherwise
-    private fun averageLessThanMedian(listOfNumbers: List<Double>): Boolean {
-        val avg = listOfNumbers.average()
-        val sortedList = listOfNumbers.sorted()
-        val median = if (sortedList.size % 2 == 0)
-            (sortedList[sortedList.size / 2] + sortedList[(sortedList.size - 1) / 2]) / 2
-        else
-            sortedList[sortedList.size / 2]
-
-        return avg < median
+    private fun averageLessThanMedian(listOfNumbers: List<Double>) = if (listOfNumbers.size % 2 == 0) {
+        listOfNumbers.average() < (listOfNumbers.sorted()[listOfNumbers.size/2] + listOfNumbers.sorted()[(listOfNumbers.size - 1) / 2] / 2)
+    } else {
+        listOfNumbers.average() < listOfNumbers.sorted()[listOfNumbers.size / 2]
     }
 
     // Create a view from an item in a collection, but recycle if possible (similar to an AdapterView's adapter)
